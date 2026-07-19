83842f286d045419e54a15c343c463317d706ea2
Z4MDAR <7ama.511.kalary@gmail.com>
2026-07-19 04:54:53 +0300

    Update bot connection details in index.js

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ setInterval(() => {
 // U CAN ONLY EDIT THIS SECTION!!
 function createBot () {
 const bot = mineflayer.createBot({
-  host: 'rubin-free.falixserver.net', 
+  host: 'kurdosmp.net', 
   version: false, // U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
-  username: 'Zoya69', 
-  port: 55178, 
+  username: 'Z4MDAR', 
+  port: 19132, 
   plugins: [AutoAuth],
   AutoAuth: 'bot112022'
 })
